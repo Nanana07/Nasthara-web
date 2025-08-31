0a2effbe971c39bf8e0ee1d9ad49b20b58fd9529
Nanda Puspitasari <nandaarch.adn@gmail.com>
2025-08-31 07:48:50 +0000

    Logonya kok tetep ga bisa ya?

--- src/components/ui/shopee-icon.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react"
-import Image from "next/image"
-
-export const Logo = (props: { className?: string }) => (
-    <Image src="/nasthara.png" alt="Nasthara Logo" width={32} height={32} {...props} />
-)
