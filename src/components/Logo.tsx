cf5647989f5eb47c13d87119dbf77fe4af1cbfd9
Nanda Puspitasari <nandaarch.adn@gmail.com>
2025-08-31 10:43:30 +0000

    Kamu paham kan kalo letak logonya di /public/Nasthara.png? Ga salah kan?

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,5 +2,5 @@ import Image from 'next/image';
 import type { FC } from 'react';
 
 export const Logo: FC<{ className?: string, style?: React.CSSProperties }> = ({ className, style }) => (
-    <Image src="/nasthara.png" alt="Nasthara Logo" width={32} height={32} className={className} style={style} />
+    <Image src="/Nasthara.png" alt="Nasthara Logo" width={32} height={32} className={className} style={style} />
 );
