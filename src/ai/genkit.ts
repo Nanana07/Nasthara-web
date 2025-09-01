1dd059ef3e830c36131dd7f6c108293085fd5de2
Nanda Puspitasari <nandaarch.adn@gmail.com>
2025-09-01 00:24:50 +0000

    Tetep ga bisa, butuh gemini API kah?

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,12 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import {config} from 'dotenv';
+
+config({path: `.env`});
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({
+    apiKey: process.env.GEMINI_API_KEY
+  })],
   model: 'googleai/gemini-1.5-flash-latest',
 });
